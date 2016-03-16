ac789a7c47e62601d607c8dbc3f86477e568c449
Eduardo Jönnerstig <eduardo_jonnerstig@live.com>
2016-03-16 11:02:14 +0100

    Included files that Visual Studio omitted from latest push.

---
+++ src/Sandbox/client/src/components/directives/fm-fund-gallery-directive/fm-fund-gallery-directive.js
@@ -0,0 +1,28 @@
+﻿(function() {
+    'use strict';
+
+    angular.module('fm').directive('fmFundGallery', ['$fmDataService', function ($fmDataService) {
+
+      return {
+        restrict: 'E',
+        templateUrl: '../../../views/fm-fund-gallery-view.html',
+        link: function (scope, elem, attrs) {
+
+          scope.title = 'Fonder';
+          
+          scope.showCard = true;
+
+          scope.getNumber = function (number) {
+            return new Array(number);
+          };
+
+          $fmDataService.then(function (data) {
+            scope.funds = data.funds;
+         });
+
+        }
+      }
+
+    }]);
+
+})();
