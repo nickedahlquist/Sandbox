10d5b9875f1a6800262feb77f36a12c9ffc50e24
Eduardo Jönnerstig <eduardo_jonnerstig@live.com>
2016-03-22 09:54:54 +0100

    Added a "utility functions" factory

---
+++ src/Sandbox/client/src/components/services/fm-utility-functions-factory.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('fm').factory('fmUtilityFunctions', [function () {
+      return {
+        intToArray: function (number) {
+          return new Array(number);
+        }
+      }
+    }]);
+
+})();
