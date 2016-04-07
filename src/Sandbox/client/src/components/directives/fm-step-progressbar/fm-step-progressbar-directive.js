ba0a1a59476014dde5925bd202bc75f37293c842
Eduardo Jönnerstig <eduardo_jonnerstig@live.com>
2016-04-07 13:35:39 +0200

    Added missing directive file to solution

---
+++ src/Sandbox/client/src/components/directives/fm-step-progressbar/fm-step-progressbar-directive.js
@@ -0,0 +1,18 @@
+﻿(function () {
+  'use strict';
+
+  angular.module('fm').directive('fmStepProgressbar', [function () {
+
+    return {
+      restrict: 'E',
+      templateUrl: 'views/fm-step-progressbar-view.html',
+      scope: {
+        steps: "="
+      },
+      link: function (scope, element, attrs) {
+      }
+    }
+
+  }]);
+
+})();
