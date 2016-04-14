67d1bd7cef91170b604c6926b3bdd5109fc59b24
Eduardo Jönnerstig <eduardo_jonnerstig@live.com>
2016-04-14 14:46:56 +0200

    Minor changes to portfolio-generator

--- src/Sandbox/client/src/components/controllers/fm-portfolio-generator-controller/fm-portfolio-generator-controller.js
+++ src/Sandbox/client/src/components/controllers/fm-portfolio-generator-controller/fm-portfolio-generator-controller.js
@@ -91,7 +91,7 @@
     }
 
     vm.preventNonNumbers = function ($event) {
-      var permittedKeys = [8, 16, 46, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106];
+      var permittedKeys = [8, ,13, 16, 46, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106];
 
       if (_.contains(permittedKeys, $event.keyCode)) {
         return;
@@ -105,7 +105,7 @@
     vm.payOnce = false;
 
     vm.togglePayMethod = function () {
-      vm.payOnce = !vm.payOnce;
+      vm.payOnce = !vm.payOnce; 
     };
 
     vm.states = {
